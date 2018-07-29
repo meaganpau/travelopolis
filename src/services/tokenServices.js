382785f83de14cb03a23aab75b8527af221b5f41
Meagan Pau <meagan.pau@gmail.com>
2018-07-29 11:57:59 -0400

    add fonts, remove password from current user response

--- src/services/tokenServices.js
+++ src/services/tokenServices.js
@@ -1,6 +1,5 @@
 export const setToken = (tokenName, token) => localStorage.setItem(tokenName, token);
 
-  
 export const getToken = tokenName => localStorage.getItem(tokenName);
 
 export const removeToken = tokenName => localStorage.removeItem(tokenName);
